7354f0c9e957e87b6ff9ca4b2e58c9c08c5741ad
Thomson <thomson@Thomsons-MacBook.local>
2024-03-02 21:55:26 +0500

    server side project update8

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,9 @@
 resource "aws_instance" "server" {
-  ami           = "ami-0a91cd140a1fc148a" # Ubuntu AMI for us-east-2
-  instance_type = "t2.micro"
-  subnet_id     = "subnet-0513fd176fd782647" #  subnet
-  vpc_security_group_ids = ["sg-0ed89c4f337f33716"] # Default VPC security group
+  ami                    = "ami-0f5daaa3a7fb3378b"
+  instance_type          = "t2.micro"
+  subnet_id              = "subnet-0513fd176fd782647"
+  vpc_security_group_ids = ["sg-0ed89c4f337f33716"]
+  key_name               = "dice-project-ec2" # Add this line
 
   tags = {
     Name = "ServerInstance"
